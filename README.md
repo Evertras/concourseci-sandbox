40298ba047e2e2b9f365445ebb19de6ae1fd4cc5
Evertras <bfullj@gmail.com>
2020-07-14 15:46:23 +0900

    Simple full working docker compose

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# concourseci-sandbox
+# ConcourseCI Sandbox
+
+Playing with [ConcourseCI](https://concourse-ci.org).
+
+As much is from scratch as possible for learning purposes.
+
+## Run it
+
+```bash
+./gen-keys.sh
+docker-compose up
+```
+
---
+++ gen-keys.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf ./keys
+mkdir -p ./keys
+echo "===> Creating keys..."
+
+docker run -v ${PWD}/keys:/tmp/keys/ --rm concourse/concourse generate-key -t rsa -f /tmp/keys/session_signing_key
+docker run -v ${PWD}/keys:/tmp/keys/ --rm concourse/concourse generate-key -t ssh -f /tmp/keys/tsa_host_key
+docker run -v ${PWD}/keys:/tmp/keys/ --rm concourse/concourse generate-key -t ssh -f /tmp/keys/worker_key
+
+cp ./keys/worker_key.pub ./keys/authorized_worker_keys
+
