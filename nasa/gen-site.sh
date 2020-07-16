e62fb6b15c77ba4573f8ec5411e6c7403fd346a8
Evertras <bfullj@gmail.com>
2020-07-16 14:51:50 +0900

    Display parameters at start for debugging

--- nasa/gen-site.sh
+++ nasa/gen-site.sh
@@ -3,7 +3,7 @@
 cd ${0%/*}
 
 if [ $# != 4 ]; then
-  echo 'Usage: ./gen-site.sh <title> <description> <URL>'
+  echo 'Usage: ./gen-site.sh <title> <description> <URL> <output-dir>'
   exit 1
 fi
 
@@ -11,6 +11,11 @@ TITLE=${1}
 DESCRIPTION=${2}
 URL=${3}
 OUTPUT_DIR=${4}
+
+echo "Title: ${TITLE}"
+echo "Description: ${DESCRIPTION}"
+echo "URL: ${URL}"
+echo "Output directory: ${OUTPUT_DIR}"
 
 cp Dockerfile ${OUTPUT_DIR}/
 
@@ -21,6 +26,8 @@ sed "s/{{title}}/${TITLE}/" ./site/index.html \
 IMG_URL=$(curl ${URL} \
   | grep 'meta property="og:image" content="' \
   | sed -n 's/.*content="\(.*\)".*/\1/p')
+
+echo Targeting ${IMG_URL}
 
 curl $IMG_URL -sLo $OUTPUT_DIR/site/pic.jpg
 
