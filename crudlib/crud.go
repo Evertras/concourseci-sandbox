163dd92e82abaa3a6a1ec4a50d4c10e1d1c8b626
Evertras <bfullj@gmail.com>
2020-07-17 13:36:44 +0900

    Add some simple create/retrieve for dogs

--- crudlib/crud.go
+++ crudlib/crud.go
@@ -1,12 +1,63 @@
 package crudlib
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+const hashKeyAge = "age"
+const hashKeyColor = "color"
+
 type Client struct {
+	pool *redis.Pool
 }
 
-func NewClient() *Client {
-	return &Client{}
+func NewClient(pool *redis.Pool) *Client {
+	return &Client{
+		pool,
+	}
 }
 
-func (c *Client) SayHello() string {
-	return "Hello"
+func TwoPlusTwo() int {
+	return 4
+}
+
+func dogKey(name string) string {
+	return fmt.Sprintf("dog:%s", name)
+}
+
+func (c *Client) CreateDog(name string, color string, age time.Duration) error {
+	dbClient := c.pool.Get()
+	key := dogKey(name)
+
+	dbClient.Send("MULTI")
+	dbClient.Send("HSET", key, hashKeyAge, age.String())
+	dbClient.Send("HSET", key, hashKeyColor, color)
+
+	if _, err := dbClient.Do("EXEC"); err != nil {
+		return fmt.Errorf("dbClient.Do(): %w", err)
+	}
+
+	return nil
+}
+
+func (c *Client) GetDogAge(name string) (time.Duration, error) {
+	dbClient := c.pool.Get()
+	key := dogKey(name)
+
+	ageString, err := redis.String(dbClient.Do("HGET", key, hashKeyAge))
+
+	if err != nil {
+		return 0, fmt.Errorf("dbClient.Do(): %w", err)
+	}
+
+	age, err := time.ParseDuration(ageString)
+
+	if err != nil {
+		return 0, fmt.Errorf("time.ParseDuration: %w", err)
+	}
+
+	return age, nil
 }
--- crudlib/crud_test.go
+++ crudlib/crud_test.go
@@ -1,14 +1,76 @@
 package crudlib
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
 
-func TestSaysHello(t *testing.T) {
-	c := NewClient()
+	"github.com/gomodule/redigo/redis"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
 
-	result := c.SayHello()
-	expected := "Hello"
+func TestAddsTwoPlusTwo(t *testing.T) {
+	result := TwoPlusTwo()
+	expected := 4
 
 	if result != expected {
-		t.Errorf("Got %q but expected %q", result, expected)
+		t.Errorf("Got %d but expected %d", result, expected)
+	}
+}
+
+func TestAddsDogAndRetrievesAge(t *testing.T) {
+	//t.Skip("Integration only")
+
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		Image: "redis",
+		ExposedPorts: []string{"6379/tcp"},
+		WaitingFor: wait.ForLog("Ready to accept connections"),
+	}
+	redisContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started: true,
+	})
+
+	if err != nil {
+		t.Fatal("testcontainers.GenericContainer():", err)
+	}
+
+	defer redisContainer.Terminate(ctx)
+
+	host, err := redisContainer.Host(ctx)
+
+	if err != nil {
+		t.Fatal("redisContainer.Host():", err)
+	}
+
+	port, err := redisContainer.MappedPort(ctx, "6379")
+
+	pool := &redis.Pool{
+		MaxIdle: 10,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", fmt.Sprintf("%s:%s", host, port.Port()), redis.DialConnectTimeout(time.Second))
+		},
+	}
+
+	client := NewClient(pool)
+
+	// Approximately 9 years
+	expectedAge := time.Hour * 24 * 365 * 9
+	dogName := "Genji"
+
+	err = client.CreateDog(dogName, "tan", expectedAge)
+
+	if err != nil {
+		t.Fatal("client.CreateDog():", err)
+	}
+
+	age, err := client.GetDogAge(dogName)
+
+	if age != expectedAge {
+		t.Errorf("Got %v but expected %v", age, expectedAge)
 	}
 }
